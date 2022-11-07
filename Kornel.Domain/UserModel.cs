eaad02acc54dd98af8924ab06dbf56582880c417
Kornél Tóth <tkornel23@gmail.com>
2022-11-07 09:11:52 +0100

    feat(databaseContext): Create db context, connect to database

--- Kornel.Domain/UserModel.cs
+++ Kornel.Domain/UserModel.cs
@@ -1,10 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Kornel.Domain
 {
     public class UserModel
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string EmailAddress { get; set; }
---
+++ Kornel.Persistance/DatabasrContext.cs
@@ -0,0 +1,21 @@
+﻿using Kornel.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kornel.Persistance
+{
+    public class DatabasrContext : DbContext
+    {
+       protected override void OnConfiguring(DbContextOptionsBuilder options)
+       {
+            options.UseSqlite("Filename=MyDatabase.db");
+       }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
